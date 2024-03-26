81a33c5e3043f78eeff98bf4b8230387a94857e5
Tomasz Skalniak <tomasz.skalniak@cdq.com>
2024-03-26 01:37:26 +0100

    feat: First draft

---
+++ go.mod
@@ -0,0 +1,18 @@
+module github.com/tiberium/icsmd
+
+go 1.21.4
+
+require (
+	github.com/arran4/golang-ical v0.2.7 // indirect
+	github.com/canhlinh/svg2png v0.0.0-20201124065332-6ba87c82371f // indirect
+	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
+	github.com/go-pdf/fpdf v0.8.0 // indirect
+	github.com/jessp01/gohighlight v0.21.1-7 // indirect
+	github.com/mandolyte/mdtopdf v1.5.3 // indirect
+	github.com/mcuadros/go-version v0.0.0-20190830083331-035f6764e8d2 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	golang.org/x/net v0.8.0 // indirect
+	golang.org/x/sys v0.6.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"os"
+
+	ics "github.com/arran4/golang-ical"
+	"github.com/mandolyte/mdtopdf"
+	"html/template"
+)
+
+type Event struct {
+	Summary     string
+	Start       string
+	Description string
+	End         string
+}
+
+func ConvertEvent(e *ics.VEvent) (*Event, error) {
+	summary := e.GetProperty("SUMMARY")
+	if summary == nil {
+		return nil, errors.New("Event does not have a summary")
+	}
+
+	start := e.GetProperty("DTSTART")
+	if start == nil {
+		return nil, errors.New("Event does not have a start time")
+	}
+
+	end := e.GetProperty("DTEND")
+	if end == nil {
+		return nil, errors.New("Event does not have an end time")
+	}
+
+	description := e.GetProperty("DESCRIPTION")
+	desc := ""
+	if description != nil {
+		desc = description.Value
+	}
+
+	return &Event{
+		Summary:     summary.Value,
+		Start:       start.Value,
+		End:         end.Value,
+		Description: desc,
+	}, nil
+}
+
+func handleErr(err error) {
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func main() {
+	icsFileName := flag.String("ics-file", "ics.ics", "The calendar file to convert")
+	outputFileName := flag.String("md-file", "ics.md", "The output file to write the markdown to")
+	renderPDF := flag.String("pdf-file", "", "If set, will render the markdown to a PDF file with the given name")
+	mkTemplateFileName := flag.String("md-template", "mk.tmpl", "The go http/template file to render the output markdown")
+	flag.Parse()
+
+	file, err := os.Open(*icsFileName)
+	handleErr(err)
+
+	cal, err := ics.ParseCalendar(file)
+	handleErr(err)
+
+	mkTemplate, err := template.ParseFiles(*mkTemplateFileName)
+	handleErr(err)
+
+	events := []*Event{}
+	for _, e := range cal.Events() {
+		event, err := ConvertEvent(e)
+		handleErr(err)
+
+		events = append(events, event)
+	}
+
+	outputFile, err := os.Create(*outputFileName)
+	handleErr(err)
+
+	err = mkTemplate.Execute(outputFile, events)
+	handleErr(err)
+	outputFile.Close()
+
+	if *renderPDF != "" {
+		markdownFileContents, err := os.ReadFile(*outputFileName)
+		handleErr(err)
+		mdtopdf.NewPdfRenderer("", "", *renderPDF, "", nil, 1).Process(markdownFileContents)
+	}
+}
